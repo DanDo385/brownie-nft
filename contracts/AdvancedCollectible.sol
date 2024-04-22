a7a69762c7dc9e6585f8fc5371d8d0434d03305c
Daniel Magro <dando385@yahoo.com>
2024-04-22 17:44:07 -0400

    initial commit

---
+++ contracts/AdvancedCollectible.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
